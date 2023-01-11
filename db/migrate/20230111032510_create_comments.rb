5146fc02b2487cecbac87ee90425ce0ba23d97c2
Jie Xiang <n.jiexiang@gmail.com>
2023-01-11 11:36:57 +0800

    Created database models

---
+++ db/migrate/20230111032510_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.integer :id, null: false
+      t.text :body, null: false
+      t.integer :rating, null: false
+      t.references :post, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
