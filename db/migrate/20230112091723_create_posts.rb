8b5e6c91dc5937da98f106d168b8865b4069e6c8
Jie Xiang <n.jiexiang@gmail.com>
2023-01-12 17:26:38 +0800

    Modified all database model fields to be non-nullable

--- db/migrate/20230112091723_create_posts.rb
+++ db/migrate/20230112091723_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
-      t.string :tags
-      t.integer :rating
+      t.string :title, null: false
+      t.text :body, null: false
+      t.string :tags, null: false, array: true, default: []
+      t.integer :rating, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
