b07262806cf7d7abc5cc598506449fa81a7956b4
Jie Xiang <n.jiexiang@gmail.com>
2023-01-15 23:46:11 +0800

    Added new Post and Comment seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 user = User.create(username: "test", password: "password")
+
+post = Post.create(
+    title: "Post title",
+    body: "Post body: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    tags: ["Tag 1", "Tag 2", "Tag 3"],
+    rating: 123,
+    user_id: 1,
+)
+
+comment = Comment.create(
+    body: "Comment body: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 234,
+    post_id: 1,
+    user_id: 1,
+)
