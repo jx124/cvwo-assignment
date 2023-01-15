b9131fb336c8b2b7b4c08c02f191e31af89ecd88
Jie Xiang <n.jiexiang@gmail.com>
2023-01-15 22:53:16 +0800

    Added JWT authentication code to controllers and seeded db

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :comments
   resources :posts
-  resources :users
+  resources :users only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
 end
