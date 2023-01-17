d651c047f5989799f2eeea01e92a9f81edc1b192
Jie Xiang <n.jiexiang@gmail.com>
2023-01-18 02:00:01 +0800

    Changed user controller to reject sign ups containing existing usernames

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,18 @@ class UsersController < ApplicationController
 
   # REGISTER
   def create
-    @user = User.create(user_params)
-    if @user.valid?
-      token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token}
+    @user = User.find_by(username: params[:username])
+    if @user
+      puts "User found: #@user"
+      render json: {error: "Username already exists"}
     else
-      render json: {error: "Invalid username or password"}
+      @user = User.create(user_params)
+      if @user.valid?
+        token = encode_token({user_id: @user.id})
+        render json: {user: @user, token: token}
+      else
+        render json: {error: "Invalid username or password"}
+      end
     end
   end
 
