3d3d972847bea94a551b972b520ae1091c5ae685
Jie Xiang <n.jiexiang@gmail.com>
2023-01-19 20:52:07 +0800

    Added has_many relationship in post and user models, set models to destroy dependants when deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
+
+    has_many :post, dependent: :destroy
+    has_many :comment, dependent: :destroy
 end
