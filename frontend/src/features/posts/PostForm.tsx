bba3c990149fff7e08b142a5e90a861e460dae58
Jie Xiang <n.jiexiang@gmail.com>
2023-01-21 17:08:22 +0800

    Styled Create Post page and added more input validations

--- frontend/src/features/posts/PostForm.tsx
+++ frontend/src/features/posts/PostForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../app/store';
-import { useFieldArray, useForm } from "react-hook-form";
+import { FieldArrayWithId, useFieldArray, useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 import { createPostAsync, CreatePostRequest, PostFormInput } from './postSlice';
@@ -10,110 +10,182 @@ import { selectAuthData } from '../auth/authSlice';
 import { useNavigate } from 'react-router-dom';
 
 const tagSchema = yup.object({
-  item: yup.string().min(1).max(30).required(),
+    item: yup
+        .string()
+        .max(30, "Tag cannot exceed 30 characters")
+        .strict()
+        .required("Tag cannot be empty"),
 })
 
 const postSchema = yup.object({
-  title: yup.string().min(5).max(50).required(),
-  body: yup.string().min(5).max(500).required(),
-  tags: yup.array().of(tagSchema).min(1).max(4),
+    title: yup
+        .string()
+        .min(5, "Title must contain at least 5 characters")
+        .max(100, "Title cannot exceed 100 characters")
+        .strict()
+        .required("A post title is required"),
+    body: yup
+        .string()
+        .min(5, "Body must contain at least 5 characters")
+        .max(500, "Body cannot exceed 500 characters")
+        .strict()
+        .required("A post body is required"),
+    tags: yup
+        .array()
+        .of(tagSchema)
+        .test("unique", "Tags need to be unique", values => {
+            return (new Set(values?.map(obj => obj.item))).size === values?.length;
+        })
+        .min(1)
+        .max(4)
+        .strict()
+        .required(),
 })
 
 function PostForm() {
-  const dispatch = useDispatch<AppDispatch>();
-  const authData = useAppSelector(selectAuthData);
-  const navigate = useNavigate();
+    const dispatch = useDispatch<AppDispatch>();
+    const authData = useAppSelector(selectAuthData);
+    const navigate = useNavigate();
 
-  const { register, control, handleSubmit, reset, formState: { errors } } = useForm<PostFormInput>({
-    resolver: yupResolver(postSchema),
-    defaultValues: {
-      title: "Post title",
-      body: "Post body.",
-      tags: [
-        { item: "Tag 1" },
-        { item: "Tag 2" },
-        { item: "Tag 3" },
-      ]
-    }
-  });
+    const { register, control, handleSubmit, formState: { errors } } = useForm<PostFormInput>({
+        resolver: yupResolver(postSchema),
+        defaultValues: {
+            title: "Post title",
+            body: "Post body",
+            tags: [
+                { item: "General" },
+            ]
+        }
+    });
 
-  const { fields, append, remove } = useFieldArray({
-    name: "tags",
-    control
-  });
+    const { fields, append, remove } = useFieldArray({
+        name: "tags",
+        control
+    });
 
-  async function onSubmit(data: PostFormInput) {
-    console.log("data: ", data);
-    console.log("token: ", authData.token);
-    /* Convert from
-
-        tags: {
-            item: string;
-        }[]
-
-    object array in PostFormInput to 
-
-        tags: string[]
+    async function onSubmit(data: PostFormInput) {
+        console.log("data: ", data);
+        console.log("token: ", authData.token);
+        /* Convert from
     
-    string array to match backend api
-    */
-    let newTagsArray: string[] = [];
+            tags: {
+                item: string;
+            }[]
+    
+        object array in PostFormInput to 
+    
+            tags: string[]
+        
+        string array to match backend api
+        */
+        let newTagsArray: string[] = [];
 
-    for (let index in data.tags) {
-      newTagsArray.push(data.tags[index].item);
+        for (let index in data.tags) {
+            newTagsArray.push(data.tags[index].item);
+        }
+
+        const createPostRequestData: CreatePostRequest = {
+            post: {
+                title: data.title,
+                body: data.body,
+                tags: newTagsArray,
+                rating: 0,
+                user_id: authData.user?.id ? authData.user?.id : 0,
+            },
+            token: authData.token ? authData.token : "",
+        }
+        console.log("request: ", createPostRequestData);
+
+        await dispatch(createPostAsync(createPostRequestData))
+            .then((response) => {
+                console.log("response: ", response);
+                // redirect to previous page if login successful
+                if (!("error" in response.payload)) {
+                    navigate(-1);
+                }
+                return response;
+            })
     }
 
-    const createPostRequestData: CreatePostRequest = {
-      post: {
-        title: data.title,
-        body: data.body,
-        tags: newTagsArray,
-        rating: 0,
-        user_id: authData.user?.id ? authData.user?.id : 0,
-      },
-      token: authData.token ? authData.token : "",
+    function renderTags(fields: FieldArrayWithId<PostFormInput, "tags", "id">[]) {
+        return fields.map((field, index) => {
+            return (
+                <div key={field.id} className="mb-1">
+                    <section key={field.id}>
+                        <div className='row'>
+                            <div className='col-auto'>
+                                <input type="text"
+                                    className="form-control"
+                                    placeholder='Enter tag here'
+                                    {...register(`tags.${index}.item` as const)} />
+                            </div>
+                            <div className='col-auto'>
+                                <button type="button"
+                                    className='btn btn-danger form-control'
+                                    disabled={fields.length <= 1}
+                                    onClick={() => remove(index)}>Delete</button>
+                            </div>
+                            <div className="form-text text-danger" style={{ height: "21px" }}>
+                                {errors.tags?.[index]?.item?.message}
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            )
+        })
     }
-    console.log("request: ", createPostRequestData);
 
-    await dispatch(createPostAsync(createPostRequestData))
-      .then((response) => {
-        console.log("response: ", response);
-        // redirect to previous page if login successful
-        if (!("error" in response.payload)) {
-          navigate(-1);
-      }
-      return response;
-      })
-  }
-
-  return (
-    <div className='App container'>
-      <h1>Create Post</h1>
-      <form onSubmit={handleSubmit(onSubmit)} id="postform">
-        <input type="text" placeholder='Title' {...register("title")} />
-        <p>{errors.title?.message}</p>
-        <textarea {...register("body")}>Enter post body here.</textarea>
-        <p>{errors.body?.message}</p>
-        {fields.map((field, index) => {
-          return (
-            <div key={field.id}>
-              <section key={field.id}>
-                <input type="text" placeholder='Enter tag here' {...register(`tags.${index}.item` as const)} />
-                <button type="button" onClick={() => remove(index)}>
-                  Delete
-                </button>
-                <p>{errors.tags?.[index]?.message}</p>
-
-              </section>
-            </div>
-          )
-        })}
-        <button type="button" onClick={() => append({ item: "New Tag" })}>Add Tag</button>
-        <input type="submit" value="Submit" />
-      </form>
-
-    </div>
-  )
+    return (
+        <div className='App container'>
+            <form onSubmit={handleSubmit(onSubmit)} id="postform">
+                <div className='card' style={{ margin: "5em" }}>
+                    <div className='card-body text-start'>
+                        <h1>Create Post</h1>
+                        <div className='mb-1'>
+                            <label htmlFor="formTitle" className="form-label fs-5">Title</label>
+                            <input type="text"
+                                className="form-control"
+                                id="formTitle"
+                                {...register("title")} />
+                            <div className="form-text text-danger" style={{ height: "21px" }}>
+                                {errors.title?.message}
+                            </div>
+                        </div>
+                        <div className='mb-1'>
+                            <label htmlFor="formBody" className="form-label fs-5">Body</label>
+                            <textarea className="form-control"
+                                id="formBody"
+                                style={{ height: "100px" }}
+                                {...register("body")}>Enter post body here.</textarea>
+                            <div className="form-text text-danger" style={{ height: "21px" }}>
+                                {errors.body?.message}
+                            </div>
+                        </div>
+                        <div className='mb-1'>
+                            <label htmlFor="formTags" className="form-label fs-5">Tags</label>
+                            {renderTags(fields)}
+                            <div className="form-text text-danger" style={{ height: "21px" }}>
+                                {errors.tags?.message}
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className="col-auto pe-0">
+                                <button type="button"
+                                    className='btn btn-secondary form-control'
+                                    disabled={fields.length >= 4}
+                                    onClick={() => append({ item: "New Tag" })}>Add Tag</button>
+                            </div>
+                            <div className="col-auto">
+                                <input type="submit"
+                                    className="btn btn-primary form-control"
+                                    value="Submit" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default PostForm
