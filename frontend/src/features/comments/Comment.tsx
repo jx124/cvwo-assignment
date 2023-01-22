3bdf5584ab00c2fc5717837b3e55bd974532f095
Jie Xiang <n.jiexiang@gmail.com>
2023-01-23 01:28:13 +0800

    Added human readable timestamps to posts and comments

--- frontend/src/features/comments/Comment.tsx
+++ frontend/src/features/comments/Comment.tsx
@@ -4,9 +4,11 @@ import { CommentProp } from './commentSlice'
 
 function Comment({ data }: CommentProp) {
     const comment = data;
-    const commentTime = new Date(data.created_at ? data.created_at : 0).getTime();
+    const commentCreatedTime = new Date(comment.created_at ? comment.created_at : 0).getTime();
+    const commentUpdatedTime = new Date(comment.updated_at ? comment.updated_at : 0).getTime();
     const currentTime = new Date().getTime();
-    const offset = currentTime - commentTime;
+    const createdOffset = currentTime - commentCreatedTime;
+    const updatedOffset = currentTime - commentUpdatedTime;
 
     return (
         <div className="card text-start px-3 py-2 m-4" key={comment.id}>
@@ -14,18 +16,26 @@ function Comment({ data }: CommentProp) {
                 <div className='col-auto pe-1'>
                     <p className='fw-bold'>{comment.user_id}</p>
                 </div>
-                <div className='col-auto ps-1'>
-                    <p className='text-secondary'>{" · " + humanReadableDuration(offset) + " ago"}</p>
+                <div className='col-auto ps-1 pe-0'>
+                    <p className='text-secondary'>{" · \u00A0" + humanReadableDuration(createdOffset) + " ago"}</p>
                 </div>
+                {createdOffset !== updatedOffset &&
+                    <div className='col-auto ps-2'>
+                        <p className='text-secondary'>
+                            <p className='text-secondary fst-italic'>
+                                {" · \u00A0edited " + humanReadableDuration(updatedOffset) + " ago"}
+                            </p>
+                        </p>
+                    </div>}
                 <div className='col-auto ms-auto'>
                     <button className='btn btn-outline-secondary btn-sm dropdown-toggle' data-bs-toggle="dropdown">...</button>
                     <ul className='dropdown-menu dropdown-menu-end'>
                         <li className='dropdown-item'
-                            // onClick={() => linkTo(`/posts/edit/?post_id=${post.id}`)}
-                            >Edit</li>
-                        <li className='dropdown-item text-danger' 
-                            // onClick={handleDeleteClick}
-                            >Delete</li>
+                        // onClick={() => linkTo(`/posts/edit/?post_id=${post.id}`)}
+                        >Edit</li>
+                        <li className='dropdown-item text-danger'
+                        // onClick={handleDeleteClick}
+                        >Delete</li>
                     </ul>
                 </div>
             </div>
--- frontend/src/features/posts/Post.tsx
+++ frontend/src/features/posts/Post.tsx
@@ -4,13 +4,19 @@ import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import { AppDispatch } from '../../app/store';
 import { selectAuthData } from '../auth/authSlice';
-import ButtonGroup from "./ButtonGroup";
+import { humanReadableDuration } from '../utils/humanReadableDuration';
 import { destroyPostAsync } from './postSlice';
 
 function Post(props: any) { // TODO: fix any type
     const post = props.post;
     const clickable = props.clickable;
     const [shadow, setShadow] = useState("");
+
+    const postCreatedTime = new Date(post.created_at ? post.created_at : 0).getTime();
+    const postUpdatedTime = new Date(post.updated_at ? post.updated_at : 0).getTime();
+    const currentTime = new Date().getTime();
+    const createdOffset = currentTime - postCreatedTime;
+    const updatedOffset = currentTime - postUpdatedTime;
 
     const navigate = useNavigate();
     const linkTo = (link: string) => {
@@ -49,10 +55,24 @@ function Post(props: any) { // TODO: fix any type
             onClick={() => { clickable && linkTo(`/posts/?post_id=${post.id}`) }}>
 
             <div className='row'>
-                <div className='col-auto'>
+                <div className='col-auto pe-0'>
                     <h3>{post.title}</h3>
                 </div>
-                {!clickable && 
+                <div className='col-auto pt-2 ps-2 pe-1'>
+                    <p className='text-secondary'>
+                        {" · \u00A0"}Posted by {post.user_id} {humanReadableDuration(createdOffset)} ago
+                    </p>
+                </div>
+                {createdOffset !== updatedOffset &&
+                    <div className='col-auto ps-1 pt-2'>
+                        <p className='text-secondary'>
+                            <p className='text-secondary fst-italic'>
+                                {" · \u00A0edited " + humanReadableDuration(updatedOffset) + " ago"}
+                            </p>
+                        </p>
+                    </div>
+                }
+                {!clickable &&
                     <div className='col-auto ms-auto'>
                         <button className='btn btn-outline-secondary dropdown-toggle' data-bs-toggle="dropdown">...</button>
                         <ul className='dropdown-menu dropdown-menu-end'>
