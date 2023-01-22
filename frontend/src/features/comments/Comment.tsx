9f3ce9660525b68dfd7bc7fd8c0a1be8bf366f0e
Jie Xiang <n.jiexiang@gmail.com>
2023-01-23 00:50:33 +0800

    Created /utils folder and moved humanReadableDuration function there

--- frontend/src/features/comments/Comment.tsx
+++ frontend/src/features/comments/Comment.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { humanReadableDuration } from '../utils/humanReadableDuration';
 import { CommentProp } from './commentSlice'
 
 function Comment({ data }: CommentProp) {
@@ -6,36 +7,6 @@ function Comment({ data }: CommentProp) {
     const commentTime = new Date(data.created_at ? data.created_at : 0).getTime();
     const currentTime = new Date().getTime();
     const offset = currentTime - commentTime;
-
-    function humanReadableDuration(milliseconds: number) {
-        function numberEnding(number: number) {
-            return (number > 1) ? 's' : '';
-        }
-        const seconds = Math.floor(milliseconds / 1000);
-
-        const years = Math.floor(seconds / 31536000);
-        if (years) {
-            return years + ' year' + numberEnding(years);
-        }
-        const months = Math.floor(seconds / 2592000);
-        if (years) {
-            return months + ' month' + numberEnding(months);
-        }
-        const days = Math.floor(seconds / 86400);
-        if (days) {
-            return days + ' day' + numberEnding(days);
-        }
-        const hours = Math.floor(seconds / 3600);
-        if (hours) {
-            return hours + ' hour' + numberEnding(hours);
-        }
-        const minutes = Math.floor(seconds / 60);
-        if (minutes) {
-            return minutes + ' minute' + numberEnding(minutes);
-        }
-        return seconds + ' second' + numberEnding(seconds);
-    }
-
 
     return (
         <div className="card text-start px-3 py-2 m-4" key={comment.id}>
@@ -45,6 +16,17 @@ function Comment({ data }: CommentProp) {
                 </div>
                 <div className='col-auto ps-1'>
                     <p className='text-secondary'>{" · " + humanReadableDuration(offset) + " ago"}</p>
+                </div>
+                <div className='col-auto ms-auto'>
+                    <button className='btn btn-outline-secondary btn-sm dropdown-toggle' data-bs-toggle="dropdown">...</button>
+                    <ul className='dropdown-menu dropdown-menu-end'>
+                        <li className='dropdown-item'
+                            // onClick={() => linkTo(`/posts/edit/?post_id=${post.id}`)}
+                            >Edit</li>
+                        <li className='dropdown-item text-danger' 
+                            // onClick={handleDeleteClick}
+                            >Delete</li>
+                    </ul>
                 </div>
             </div>
             <div className='mb-1'>
---
+++ frontend/src/features/utils/humanReadableDuration.ts
@@ -0,0 +1,28 @@
+export function humanReadableDuration(milliseconds: number) {
+    function numberEnding(number: number) {
+        return (number > 1) ? 's' : '';
+    }
+    const seconds = Math.floor(milliseconds / 1000);
+
+    const years = Math.floor(seconds / 31536000);
+    if (years) {
+        return years + ' year' + numberEnding(years);
+    }
+    const months = Math.floor(seconds / 2592000);
+    if (years) {
+        return months + ' month' + numberEnding(months);
+    }
+    const days = Math.floor(seconds / 86400);
+    if (days) {
+        return days + ' day' + numberEnding(days);
+    }
+    const hours = Math.floor(seconds / 3600);
+    if (hours) {
+        return hours + ' hour' + numberEnding(hours);
+    }
+    const minutes = Math.floor(seconds / 60);
+    if (minutes) {
+        return minutes + ' minute' + numberEnding(minutes);
+    }
+    return seconds + ' second' + numberEnding(seconds);
+}
